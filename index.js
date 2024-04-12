98bf2e2587186b66018a59557cbad81b2ffc152e
Keuznub <breixopastoriza@gmail.com>
2024-04-12 10:07:38 +0200

    Version con bootstrap modal y js Bien hecho con titulos

--- index.js
+++ index.js
@@ -1,30 +1,16 @@
 document.addEventListener("DOMContentLoaded", function () {
-    // Get the modal
-    var modal = document.getElementById("myModal");
+    var imagenCargar = document.getElementById("imagenCargar");
+    var titulo = document.getElementById("staticBackdropLabel");
+    var imagenes = document.getElementsByTagName("img");
+    var imagenesArray = Array.from(imagenes);
 
-    // Get the image and insert it inside the modal - use its "alt" text as a caption
-    var img = document.getElementsByTagName("img");
-    var modalImg = document.getElementById("img01");
-    var captionText = document.getElementById("caption");
-    var arrrayImg = Array.from(img)
-
-    arrrayImg.forEach(function (imagen) {
-        imagen.onclick = function () {
-            modal.style.display = "block";
-            modalImg.src = this.src;
-            captionText.innerHTML = this.alt;
-        }
+    imagenesArray.forEach(function(imagen) {   
+        imagen.addEventListener("click", (evento)=>{ 
+            imagenCargar.src = evento.target.src;
+            imagenCargar.alt = evento.target.alt;
+            titulo.innerHTML = evento.target.alt;
+        })
     });
-
-
-    // Get the <span> element that closes the modal
-    var span = document.getElementsByClassName("close")[0];
-
-    // When the user clicks on <span> (x), close the modal
-    span.onclick = function () {
-        modal.style.display = "none";
-    }
-
 });
 
 
