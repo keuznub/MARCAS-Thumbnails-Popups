d2d86403169c3608b7fe33248c51dafe921f36cc
Keuznub <breixopastoriza@gmail.com>
2024-04-15 13:26:05 +0200

    Funcionando, cambios en java, foreach imagenes e iconos

--- index.js
+++ index.js
@@ -1,27 +1,3 @@
-document.addEventListener("DOMContentLoaded", function () {
-
-    //Pop-up
-    var imagenCargar = document.getElementById("imagenCargar");
-    var titulo = document.getElementById("staticBackdropLabel");
-    var imagenes = document.querySelectorAll("a img")
-    var imagenesArray = Array.from(imagenes);
-
-    imagenesArray.forEach(function(imagen) {   
-        imagen.setAttribute("megusta", "false");
-        imagen.addEventListener("click", (evento)=>{ 
-            imagenCargar.src = evento.target.src;
-            imagenCargar.alt = evento.target.alt;
-            titulo.innerHTML = evento.target.alt;
-            if(evento.target.megusta == "true"){
-                var megustaText = document.getElementById("megustaText");
-                megustaText.style.display = "block";
-            }
-        })
-    });
-
-});
-
-
 const corazonSprite = [
     "Imagenes/corazon0.png",
     "Imagenes/corazon1.png",
@@ -35,18 +11,78 @@ const corazonSprite = [
     "Imagenes/corazon4.png"
 ]  
 
-var iconosCorazon = document.getElementsByClassName("iconosCorazon");
-var iconosCorazonArray = Array.from(iconosCorazon);
-var imagenes = document.querySelectorAll("a img")
-var imagenesArray = Array.from(imagenes);
+
+document.addEventListener("DOMContentLoaded", function () {
+
+    //Pop-up
+    var imagenCargar = document.getElementById("imagenCargar");
+    var titulo = document.getElementById("staticBackdropLabel");
+    var imagenes = document.querySelectorAll("a img")
+    var imagenesArray = Array.from(imagenes);
+    var iconosCorazon = document.getElementsByClassName("iconosCorazon");
+    var iconosCorazonArray = Array.from(iconosCorazon);
+    var contador1 = 0;
 
 
-iconosCorazonArray.forEach(function(ico){
-    ico.src = corazonSprite[0];
-    ico.addEventListener("click", (event)=>{
-        cambiarIco(event.target);
+    imagenesArray.forEach(function(imagen) {   
+        imagen.setAttribute("megusta", "false");
+        imagen.addEventListener("click", (evento)=>{ 
+            imagenCargar.src = evento.target.src;
+            imagenCargar.alt = evento.target.alt;
+            titulo.innerHTML = evento.target.alt;
+            if(evento.target.getAttribute("megusta") == "true"){
+                console.log("es true!!!");
+                var megustaText = document.getElementById("megustaText");
+                megustaText.style.display = "block";
+            }else{
+                console.log("es false!!!" + evento.target.getAttribute("megusta"));
+                var megustaText = document.getElementById("megustaText");
+                megustaText.style.display = "none";
+            }
+        })
+    });
+
+    iconosCorazonArray.forEach(function(ico) {
+        var imagen = imagenesArray[contador1];
+        ico.src = corazonSprite[0];
+        console.log("iteracion: " + " icono: " + ico + " imagen:  " + imagen);
+        ico.addEventListener("click", (event)=>{
+            cambiarIco(event.target);      
+            if(imagen.getAttribute("megusta") == "false"){
+                imagen.setAttribute("megusta" , "true");
+            }else{
+                imagen.setAttribute("megusta" , "false");
+            }
+            console.log("Atributo megusta de la imagen : " + imagen.getAttribute("megusta"));
+        })
+        contador1++;
+    });
+
+
+
+
+/*
+    for(var i = 0; i<iconosCorazonArray.length;i++){
+        var ico = iconosCorazonArray[i];
+        var imagen = imagenesArray[i];
+        ico.src = corazonSprite[0];
+        console.log("iteracion: " + i + " icono: " + ico + " imagen:  " + imagen);
+        ico.addEventListener("click", (event)=>{
+            cambiarIco(event.target);      
+            if(imagen.getAttribute("megusta") == "false"){
+                imagen.setAttribute("megusta" , "true");
+            }else{
+                imagen.setAttribute("megusta" , "false");
+            }
+            console.log("Atributo megusta de la imagen "+ i + ": " + imagen.getAttribute("megusta"));
     })
-})
+    }
+*/
+
+});
+
+
+
 
 function cambiarIco(ico){
     let rutaRelativaIco = obtenerRutaRelativa(ico.src);
@@ -58,7 +94,6 @@ function cambiarIco(ico){
         let intervalo = setInterval(() => {
             contador++; 
             ico.src = corazonSprite[contador];
-    
             if (contador >= corazonSprite.length-1) { 
                 clearInterval(intervalo);
             }
